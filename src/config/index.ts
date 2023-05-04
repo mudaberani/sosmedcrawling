240888bb0060e8f3d8e64e55785dd4f2ad1111bc
dikahariss <dika.webmaster@gmail.com>
2023-05-04 17:56:54 +0700

    update config instagram

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,14 @@ dotenv.config();
 
 export const config = {
   instagram: {
+    isInstagram: process.env.IS_INSTAGRAM === 'true',
     username: process.env.INSTAGRAM_USERNAME!,
     password: process.env.INSTAGRAM_PASSWORD!,
+    profile: process.env.INSTAGRAM_PROFILE!,
+    hashtag: process.env.INSTAGRAM_HASHTAG!,
+    totalPosts: Number(process.env.INSTAGRAM_TOTAL_POSTS!),
+    isProfile: process.env.INSTAGRAM_IS_PROFILE === 'true',
+    isHashtag: process.env.INSTAGRAM_IS_HASHTAG === 'true',
   },
   viewport: {
     width: 1920,
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import InstagramCrawler from './services/InstagramCrawler';
 import FileStorage from './storage/FileStorage';
+import { config } from './config';
 
 (async () => {
   const storage = new FileStorage();
   const Instagram = new InstagramCrawler(storage);
+  const { isInstagram } = config.instagram;
 
-  await Instagram.init();
-  await Instagram.login();
-  await Instagram.dismissNotNowButtons();
-  await Instagram.crawlHashtag('aniesbaswedan', 1000);
-  await Instagram.close();
+  if (isInstagram) {
+    const { isHashtag } = config.instagram;
+
+    if (isHashtag) {
+      const { hashtag, totalPosts } = config.instagram;
+      await Instagram.init();
+      await Instagram.login();
+      await Instagram.dismissNotNowButtons();
+      await Instagram.crawlHashtag(hashtag, totalPosts);
+      await Instagram.close();
+    }
+  }
 })();
